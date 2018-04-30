5f2d2d35f393aba859ac4ee0d4009afb1329defe
Yeonsoo Yoo <carlos@nexon.co.kr>
2018-04-30 15:04:04 +0900

    Deploy every time when env is development.

--- src/util/getContract.js
+++ src/util/getContract.js
@@ -1,13 +1,21 @@
-import Web3 from 'web3'
 import {default as contract} from 'truffle-contract'
 import treasure_artifacts from '../../build/contracts/Treasure.json'
 
 let getContract = new Promise(function (resolve, reject) {
     let Treasure = contract(treasure_artifacts);
     Treasure.setProvider(web3.currentProvider);
-    let address = "0xdb8c2c51566c2dc71d4aa1a334eb991ffb9221bf"
-    let treausreContractInstance = Treasure.at(address)
-    resolve(treausreContractInstance)
+
+    if (process.env.NODE_ENV === "development") {
+        Treasure.deployed().then(function (instance) {
+            resolve(instance);
+        });
+    } else {
+        let address = "YOU MUST SET ADDRESS OR TREASURE"
+        let treasureContractInstance = Treasure.at(address)
+        resolve(treasureContractInstance)
+    }
+
+
 })
 
 export default getContract
